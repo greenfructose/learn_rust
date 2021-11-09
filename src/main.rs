78fa2614717045632c23044afee0899935d91324
greenfructose <justinclarkturney@gmail.com>
2021-11-09 09:46:46 -0600

    Practicing enums and pattern matching

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
+
+
     let scale = 2;
     let rect1 = Rectangle {
         width: 30,
@@ -118,6 +120,46 @@ fn guessing_game() {
         }
     }
 }
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) ->u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) -> i32{
+        42
+    }
+}
+
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String
+}
+
 struct User {
     active: bool,
     username: String,
